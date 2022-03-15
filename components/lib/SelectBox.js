275dbee7634ac947386c509acc2b0d186630272b
Raj kumar singh <raj.swd1@gmail.com>
2022-03-15 16:32:31 +0800

    Update SelectBox.js

--- components/lib/SelectBox.js
+++ components/lib/SelectBox.js
@@ -1,6 +1,8 @@
 import * as React from "react";
 import styles from "../../commonStyle";
 
+const countryList = ["US", "UK", "India", "China", "Malaysia", "Singapore", "Japan", "Thailand","Srilanka"];
+
 const SelectBox = ({ handler }) => (
   <div>
     <label  className="label">Nationality:</label>
@@ -8,10 +10,9 @@ const SelectBox = ({ handler }) => (
       <option value="" disabled>
         Select
       </option>
-      <option value="us">US</option>
-      <option value="uk">UK</option>
-      <option value="india">India</option>
-      <option value="china">China</option>
+      {
+        countryList.map(val => <option value={val}>{val}</option>
+      )}
     </select>
   </div>
 );
